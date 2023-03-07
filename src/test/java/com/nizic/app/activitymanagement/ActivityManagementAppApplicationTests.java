427058a656f71ae9bb3fa0f2ed2ecd47276fdc49
mnizic <marionizic014@gmail.com>
2023-03-07 17:53:39 +0100

    Initial commit

---
+++ src/test/java/com/nizic/app/activitymanagement/ActivityManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nizic.app.activitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
