a42aafb87c7082402e75bf3619a2413e19ea6cf7
mnizic <marionizic014@gmail.com>
2023-03-08 16:49:38 +0100

    Renamed project.

--- src/test/java/com/nizic/app/eventmanagment/EventManagementAppApplicationTests.java
+++ src/test/java/com/nizic/app/eventmanagment/EventManagementAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nizic.app.activitymanagement;
+package com.nizic.app.eventmanagment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ActivityManagementAppApplicationTests {
+class EventManagementAppApplicationTests {
 
 	@Test
 	void contextLoads() {
