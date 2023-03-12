976d644687472f548cc93e5a656ca2fc610f9f6a
mnizic <marionizic014@gmail.com>
2023-03-12 04:38:51 +0100

    Minor changes.

--- src/main/java/com/nizic/app/eventmanagment/HomePageController.java
+++ src/main/java/com/nizic/app/eventmanagment/HomePageController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HomePageController {
     @GetMapping("")
-    public String showHomePage(){
+    public String showHomePage() {
         return "index";
     }
 }
