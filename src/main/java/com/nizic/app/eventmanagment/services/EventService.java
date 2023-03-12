e1529ed82f084d59578e6e26b79a55ceba34c6cd
mnizic <marionizic014@gmail.com>
2023-03-12 22:21:22 +0100

    Added Edit, Delete and Search.

--- src/main/java/com/nizic/app/eventmanagment/services/EventService.java
+++ src/main/java/com/nizic/app/eventmanagment/services/EventService.java
@@ -7,14 +7,29 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventService {
-    @Autowired private EventRepository repo;
-    public List<Event> listAllEvents(){
+    @Autowired
+    private EventRepository repo;
+
+    public List<Event> listAllEvents(String keyword) {
+        if (keyword != null) return (List<Event>) repo.findAll(keyword);
         return (List<Event>) repo.findAll();
     }
 
+    public List<Event> listFromBetweenEvents(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        return (List<Event>) repo.findByFromBetween(startDateTime, endDateTime);
+    }
+
+    public List<Event> listToBetweenEvents(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        return (List<Event>) repo.findByToBetween(startDateTime, endDateTime);
+    }
+
+    public List<Event> listFreeEntry(Boolean freeEntry) {
+        return (List<Event>) repo.findByFreeEntry(freeEntry);
+    }
 
     public void add(Event event) {
         LocalDateTime from = event.getFrom();
@@ -22,5 +37,22 @@ public class EventService {
         event.setFrom(from);
         event.setTo(to);
         repo.save(event);
+    }
+
+    public Event get(Long id) throws Exception {
+        Optional<Event> result = repo.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new Exception("Event with the ID: " + id + " is not found!");
+    }
+
+    public void delete(Long id) throws Exception {
+        Long count = repo.countById(id);
+        if (count != null || count > 0) {
+            repo.deleteById(id);
+        } else {
+            throw new Exception("There are no events to delete!");
+        }
     }
 }
